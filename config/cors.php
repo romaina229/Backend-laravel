29a59644fb1e4af857648a440cee37924bfe0825
AKPO <nounonvenanromain@gmail.com>
2026-02-04 22:35:34 +0000

    correction finale

--- config/cors.php
+++ config/cors.php
@@ -16,13 +16,18 @@ return [
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+
     'allowed_origins' => [
-    'https://aquagestion-frontend.onrender.com',
-    'http://localhost:3000',],
+        'https://aquagestionpossonnerie.onrender.com',
+    ],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => true,
 ];
