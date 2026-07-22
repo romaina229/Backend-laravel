ed67e35620cc5096c94de6a0bfbba8ddb7bb0a8d
Shalom Digital Solutions <nounonvenanromain@gmail.com>
2026-07-22 23:03:55 +0100

    Change database name from 'uac_repertoire' to 'aquagestion'
    mise à jour database

--- config/database.php
+++ config/database.php
@@ -87,7 +87,7 @@ return [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'uac_repertoire'),
+            'database' => env('DB_DATABASE', 'aquagestion'),
             'username' => env('DB_USERNAME', 'postgres'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
